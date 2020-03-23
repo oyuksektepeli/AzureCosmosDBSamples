acddf8ce9e9e154f3c4be48981bbeb8bc1d765ee
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2020-03-23 09:11:33 +0300

    Add Cosmos Document

--- AzureCosmosDBSamples/CosmosWorker.cs
+++ AzureCosmosDBSamples/CosmosWorker.cs
@@ -1,7 +1,10 @@
 ﻿using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -128,6 +131,175 @@ namespace AzureCosmosDBSamples
 			await container.DeleteContainerAsync();
 
 			Console.WriteLine($"Deleted container {containerId} from database {dbname}");
+		}
+
+		public async static Task CreateDocuments()
+		{
+			Console.Clear();
+			Console.WriteLine(">>> Create Documents <<<");
+			Console.WriteLine();
+
+			var container = CosmosWorker.Client.GetContainer("mydb", "mystore");
+
+			dynamic document1Dynamic = new
+			{
+				id = Guid.NewGuid(),
+				name = "New Customer 1",
+				address = new
+				{
+					addressType = "Microsoft HQ",
+					addressLine1 = "One Microsoft Way",
+					location = new
+					{
+						city = "Redmond",
+						stateProvinceName = "Washington"
+					},
+					postalCode = "11229",
+					countryRegionName = "United States"
+				},
+			};
+
+			await container.CreateItemAsync(document1Dynamic, new PartitionKey("11229"));
+			Console.WriteLine($"Created new document {document1Dynamic.id} from dynamic object");
+
+			var document2Json = $@"
+				{{
+					""id"": ""{Guid.NewGuid()}"",
+					""name"": ""New Customer 2"",
+					""address"": {{
+						""addressType"": ""Main Office"",
+						""addressLine1"": ""123 Main Street"",
+						""location"": {{
+							""city"": ""Beverly Hills""
+							""stateProvinceName"": ""Los Angeles""
+						}},
+						""postalCode"": ""11229"",
+						""countryRegionName"": ""United States""
+					}}
+				}}";
+
+			var document2Object = JsonConvert.DeserializeObject<JObject>(document2Json);
+			await container.CreateItemAsync(document2Object, new PartitionKey("11229"));
+			Console.WriteLine($"Created new document {document2Object["id"].Value<string>()} from JSON string");
+
+			var document3Poco = new Customer
+			{
+				Id = Guid.NewGuid().ToString(),
+				Name = "New Customer 3",
+				Address = new Address
+				{
+					AddressType = "Main Office",
+					AddressLine1 = "123 Main Street",
+					Location = new Location
+					{
+						City = "Brooklyn",
+						StateProvinceName = "New York"
+					},
+					PostalCode = "11229",
+					CountryRegionName = "United States"
+				},
+			};
+
+			await container.CreateItemAsync(document3Poco, new PartitionKey("11229"));
+			Console.WriteLine($"Created new document {document3Poco.Id} from typed object");
+		}
+
+		public static async Task QueryDocuments()
+		{
+			Console.Clear();
+			Console.WriteLine(">>> Query Documents (SQL) <<<");
+			Console.WriteLine();
+
+			var container = CosmosWorker.Client.GetContainer("mydb", "mystore");
+
+			Console.WriteLine("Querying for new customer documents (SQL)");
+			var sql = "SELECT * FROM c WHERE STARTSWITH(c.name, 'New Customer') = true";
+
+			// Query for dynamic objects
+			var iterator1 = container.GetItemQueryIterator<dynamic>(sql);
+			var documents1 = await iterator1.ReadNextAsync();
+			var count = 0;
+			foreach (var document in documents1)
+			{
+				Console.WriteLine($" ({++count}) Id: {document.id}; Name: {document.name};");
+
+				// Dynamic object can be converted into a defined type...
+				var customer = JsonConvert.DeserializeObject<Customer>(document.ToString());
+				Console.WriteLine($"     City: {customer.Address.Location.City}");
+			}
+			Console.WriteLine($"Retrieved {count} new documents as dynamic");
+			Console.WriteLine();
+
+			// Or query for defined types; e.g., Customer
+			var iterator2 = container.GetItemQueryIterator<Customer>(sql);
+			var documents2 = await iterator2.ReadNextAsync();
+			count = 0;
+			foreach (var customer in documents2)
+			{
+				Console.WriteLine($" ({++count}) Id: {customer.Id}; Name: {customer.Name};");
+				Console.WriteLine($"     City: {customer.Address.Location.City}");
+			}
+			Console.WriteLine($"Retrieved {count} new documents as Customer");
+			Console.WriteLine();
+
+			// You only get back the first "page" (up to MaxItemCount)
+		}
+
+		public async static Task DeleteDocuments()
+		{
+			Console.Clear();
+			Console.WriteLine(">>> Delete Documents <<<");
+			Console.WriteLine();
+
+			var container = CosmosWorker.Client.GetContainer("mydb", "mystore");
+
+			Console.WriteLine("Querying for documents to be deleted");
+			var sql = "SELECT c.id, c.address.postalCode FROM c WHERE STARTSWITH(c.name, 'New Customer') = true";
+			var iterator = container.GetItemQueryIterator<dynamic>(sql);
+			var documents = (await iterator.ReadNextAsync()).ToList();
+			Console.WriteLine($"Found {documents.Count} documents to be deleted");
+			foreach (var document in documents)
+			{
+				string id = document.id;
+				string pk = document.postalCode;
+				await container.DeleteItemAsync<dynamic>(id, new PartitionKey(pk));
+			}
+			Console.WriteLine($"Deleted {documents.Count} new customer documents");
+			Console.WriteLine();
+		}
+
+		public async static Task ReplaceDocuments()
+		{
+			Console.Clear();
+			Console.WriteLine(">>> Replace Documents <<<");
+			Console.WriteLine();
+
+			var container = CosmosWorker.Client.GetContainer("mydb", "mystore");
+
+			Console.WriteLine("Querying for documents with 'isNew' flag");
+			var sql = "SELECT VALUE COUNT(c) FROM c WHERE c.isNew = true";
+			var count = (await (container.GetItemQueryIterator<int>(sql)).ReadNextAsync()).First();
+			Console.WriteLine($"Documents with 'isNew' flag: {count}");
+			Console.WriteLine();
+
+			Console.WriteLine("Querying for documents to be updated");
+			sql = "SELECT * FROM c WHERE STARTSWITH(c.name, 'New Customer') = true";
+			var documents = (await (container.GetItemQueryIterator<dynamic>(sql)).ReadNextAsync()).ToList();
+			Console.WriteLine($"Found {documents.Count} documents to be updated");
+			foreach (var document in documents)
+			{
+				document.isNew = true;
+				var result = await container.ReplaceItemAsync<dynamic>(document, (string)document.id);
+				var updatedDocument = result.Resource;
+				Console.WriteLine($"Updated document 'isNew' flag: {updatedDocument.isNew}");
+			}
+			Console.WriteLine();
+
+			Console.WriteLine("Querying for documents with 'isNew' flag");
+			sql = "SELECT VALUE COUNT(c) FROM c WHERE c.isNew = true";
+			count = (await (container.GetItemQueryIterator<int>(sql)).ReadNextAsync()).First();
+			Console.WriteLine($"Documents with 'isNew' flag: {count}");
+			Console.WriteLine();
 		}
 
 	}
